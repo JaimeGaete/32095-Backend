f2bf7a10c4b03dbf91ccd16acc86452548679df0
Jaime Gaete Jimenez <jaime.gaete@gmail.com>
2022-09-12 03:19:11 -0300

    Primera Entrega del Proyecto Final

--- config/configuration.js
+++ config/configuration.js
@@ -1,10 +1,7 @@
 
 const configuration = {
     PORT: 8080,
-    ADMINISTRADOR: true,
-    FILEPRODUCTOS: './files/productos.txt',
-    FILECARRITO: './files/carrito.txt'
+    ADMINISTRADOR: true
   };
-
 
 module.exports.configuration = configuration
